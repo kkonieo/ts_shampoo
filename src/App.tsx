19b4e26ea9cbd5f483b7444c381845cdd5b09a72
misun <mcoding777@naver.com>
2022-01-22 04:43:06 +0900

    Feat: 예비 글로벌 스타일 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,40 @@
 import Login from './pages/Login';
 import { Routes, Route } from 'react-router-dom';
+import { createGlobalStyle } from 'styled-components';
 
 function App() {
   return (
-    <Routes>
-      <Route path="/login" element={<Login key="" />} />
-    </Routes>
+    <>
+      <GlobalStyle />
+      <Routes>
+        <Route path="/login" element={<Login key="" />} />
+      </Routes>
+    </>
   );
 }
 
 export default App;
+
+// styled-components
+
+// 이 부분은 일단 예비용
+const GlobalStyle = createGlobalStyle`
+  * {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+    color: white;
+    text-decoration: none;
+  }
+  html {
+    font-size: 18px;
+    background-color: #F5F5F5;
+  }
+  body {   
+    text-align: center;
+  }
+  h1 {
+    font-weight: bold;
+    font-size: 2rem;
+  }
+`;
