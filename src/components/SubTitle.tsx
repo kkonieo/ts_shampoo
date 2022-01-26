bfa6d9ab2a12138647a22a083533b56123b35322
HyeongSeoku <gudtjr3437@naver.com>
2022-01-26 12:45:13 +0900

    Fix : Prettier 미적용 오류 해결

--- src/components/SubTitle.tsx
+++ src/components/SubTitle.tsx
@@ -1,21 +1,20 @@
 import styled from 'styled-components';
 
-interface Props{
-    text:string;
+interface Props {
+    text: string;
 }
 
-const SubTitle = ({text}:Props) =>{
-    return(
-        <Title>{text}</Title>
-    )
-}
+const SubTitle = ({ text }: Props) => {
+    return <Title>{text}</Title>;
+};
 
-export default SubTitle
+export default SubTitle;
 
 const Title = styled.div`
     font-size: 20px;
     font-weight: 700;
     padding-bottom: 5px;
-    border-bottom: 1px solid #BDBDBD;
+    border-bottom: 1px solid #bdbdbd;
     text-align: left;
-`
+    margin: 10px 0px;
+`;
