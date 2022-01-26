38642203b9f0ed94c981ed1f1369ed7ab8269cc1
minyoung <leemin02_@naver.com>
2022-01-26 14:20:58 +0900

    Chore: types.d.ts 파일로 interface 이동

--- src/components/home/portfolioList/PortfolioList.tsx
+++ src/components/home/portfolioList/PortfolioList.tsx
@@ -1,13 +1,10 @@
 import { PortfolioListView } from './PortfolioListView';
+import {HomeProps} from 'HomeModule';
 
 export const PortfolioList = (): JSX.Element => {
-    interface UserInfoProps {
-        name: string;
-        job: string;
-    }
 
     // 유저 포트폴리오 임의 생성을 위한 더미 데이터
-    const userInfo: UserInfoProps[] = [
+    const userInfo: HomeProps.UserInfoProps[] = [
         {
             name: '엘리스',
             job: '프론트엔드 개발자',
