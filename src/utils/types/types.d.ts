7a148b1c4b8e17c87c8e5e88c9bbe04ecaf4802d
HyeongSeoku <gudtjr3437@naver.com>
2022-01-28 18:38:15 +0900

    Feat : SkillTag 를 위한 타입 명시

--- src/utils/types/types.d.ts
+++ src/utils/types/types.d.ts
@@ -8,6 +8,10 @@ declare module 'AboutMePageModuel' {
             skillTitles: string | null;
             skillDescribes: string[] | [];
         }
+        interface SkillTagProps {
+            skill: string[];
+            skillDescriptions: { title: string; describe: string[] }[];
+        }
         interface SkillsDetailProps {
             skill: string;
             onSkillClick: any;
@@ -28,18 +32,18 @@ declare module 'AboutMePageModuel' {
 declare module 'loginModule' {
     export namespace loginSpace {
         interface LoginButtonProps {
-            type: "submit";
+            type: 'submit';
             text: string;
-            className: "blue_button" | "gray_button"; // 버튼 컬러 바꾸기용
+            className: 'blue_button' | 'gray_button'; // 버튼 컬러 바꾸기용
         }
 
         interface SnsLoginButtonProps {
-            text: "깃허브로 로그인" | "구글로 로그인" | "네이버로 로그인";
-            to: "github" | "google" | "naver"; // 네비게이터용
+            text: '깃허브로 로그인' | '구글로 로그인' | '네이버로 로그인';
+            to: 'github' | 'google' | 'naver'; // 네비게이터용
             color: string;
         }
 
-            interface LoginContainerProps {
+        interface LoginContainerProps {
             children: React.ReactNode;
         }
     }
@@ -50,7 +54,7 @@ declare module 'SignUp' {
         userEmail?: string;
         userName: string;
         userJob: string;
-    };
+    }
 }
 
 declare module 'ProjectPageModule' {
