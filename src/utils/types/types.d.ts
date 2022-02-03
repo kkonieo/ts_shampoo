c0c3678da55fe78323eb6085ebd69f840dc297a2
minyoung <leemin02_@naver.com>
2022-02-03 23:26:55 +0900

    chore: AOS 의존성 추가

--- src/utils/types/types.d.ts
+++ src/utils/types/types.d.ts
@@ -28,18 +28,18 @@ declare module 'AboutMePageModuel' {
 declare module 'loginModule' {
     export namespace loginSpace {
         interface LoginButtonProps {
-            type: "submit";
+            type: 'submit';
             text: string;
-            className: "blue_button" | "gray_button"; // 버튼 컬러 바꾸기용
+            className: 'blue_button' | 'gray_button'; // 버튼 컬러 바꾸기용
         }
 
         interface SnsLoginButtonProps {
-            text: "깃허브로 로그인" | "구글로 로그인" | "네이버로 로그인";
-            to: "github" | "google" | "naver"; // 네비게이터용
+            text: '깃허브로 로그인' | '구글로 로그인' | '네이버로 로그인';
+            to: 'github' | 'google' | 'naver'; // 네비게이터용
             color: string;
         }
 
-            interface LoginContainerProps {
+        interface LoginContainerProps {
             children: React.ReactNode;
         }
     }
@@ -50,7 +50,7 @@ declare module 'SignUp' {
         userEmail?: string;
         userName: string;
         userJob: string;
-    };
+    }
 }
 
 declare module 'ProjectPageModule' {
@@ -90,3 +90,5 @@ declare module 'HomeModule' {
         }
     }
 }
+
+declare module 'aos';
