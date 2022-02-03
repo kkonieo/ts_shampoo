40e02fc6ba36143f121832da9c9ef3dc9a45f2b7
HyeongSeoku <gudtjr3437@naver.com>
2022-02-03 15:01:30 +0900

    Feat : 수정 모드인지 판별 위한 atom 작성

--- src/utils/data/atom.tsx
+++ src/utils/data/atom.tsx
@@ -1,6 +1,16 @@
 import { atom } from 'recoil';
-  
+import { RecoilProps } from 'RecoilModule';
+
 export const pageState = atom<number>({
     key: 'pageState',
     default: 0,
+});
+
+export const aboutMeEditState = atom<RecoilProps.aboutMeEditProps[]>({
+    key: 'aboutMeEditState',
+    default: [
+        { id: 'summary', editMode: false },
+        { id: 'skills', editMode: false },
+        { id: 'resume', editMode: false },
+    ],
 });
