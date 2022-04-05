79a4e7bf205ceab6c5629dafc930b6f047110bbc
misun <mcoding777@naver.com>
2022-04-05 15:44:22 +0900

    Feat: 메뉴 톱니바퀴 아이콘 클릭 시 로그인 여부 판단

--- src/components/nav/IconDiv.tsx
+++ src/components/nav/IconDiv.tsx
@@ -2,13 +2,20 @@ import styled from 'styled-components';
 import { Link } from 'react-router-dom';
 
 const IconDiv = () => {
+    // 로그인 유저인지 확인하기
+    const userProfile: string | null = sessionStorage.getItem('userProfile');
+
+    const handleClick = () => {
+        !userProfile && alert('로그인 유저만 볼 수 있는 페이지입니다');
+    };
+
     return (
         <Div>
             <Link to="..">
                 <img src={`${process.env.PUBLIC_URL}/img/home.svg`} alt="홈으로 가기" />
             </Link>
 
-            <Link to="/nav/mypage">
+            <Link to={userProfile ? '/nav/mypage' : ''} onClick={handleClick}>
                 <img src={`${process.env.PUBLIC_URL}/img/cogOutline.svg`} alt="마이 페이지" />
             </Link>
         </Div>
